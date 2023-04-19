2af848d1cbb00bc2123313c8dbda9de154959ce2
Christiano0407 <placebo0704@gmail.com>
2023-04-19 09:07:05 -0600

    multi-page with vite

--- vite-demo/vite.config.js
+++ vite-demo/vite.config.js
@@ -1,5 +1,7 @@
 //** === ===== === Vite Config === ==== === */
 import { defineConfig, loadEnv } from 'vite';
+import { dirname, resolve } from 'path';
+const __dirname = resolve(dirname(''));
 
 export default defineConfig(({ command, mode }) => {
   const port = 3000;
@@ -13,13 +15,23 @@ export default defineConfig(({ command, mode }) => {
   /* === command & mode*/
   if (mode === 'development') {
     console.log('State Dev');
+    return {
+      server: {
+        port,
+      },
+    };
   } else {
     console.log('State Production - Build');
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, `index.html`),
+            help: resolve(__dirname, `help`, `help.html`),
+            nested: resolve(__dirname, `nested`, `nested.html`),
+          },
+        },
+      },
+    };
   }
-
-  return {
-    server: {
-      port,
-    },
-  };
 });
